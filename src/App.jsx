6039527b3bcb6a5025bea09f3360c346ebd856d1
kamal-prog <kamalk878@gmail.com>
2025-10-16 13:34:38 +0530

    https://github.com/kamal-prog/i-computers-frontend.git

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
 import './App.css'
 import ProductCard from './components/productCard'
+import Header from './components/header'
+import UserData from './components/userData'
 
 function App() {
 
   return (
-    <>
-      <ProductCard name="Laptop" price="LKR 100000" image="https://picsum.photos/id/0/367/267"/>
-
-      <ProductCard name="watch" price="LKR 40000" image="https://picsum.photos/id/4/367/267"/>
-
-      <ProductCard name="Mobile" price="LKR 30000" image="https://picsum.photos/id/5/367/267"/>
-
-
-    </>
+    <div className='boder w-[600px] h-[600px] bg-gray-400 relative'>
+      <div className='w-[500px] h-[500px] bg-yellow-300 flex flex-col justify-center items-center'>
+        <div className='w-[100px] h-[100px] bg-red-300'>  
+        </div>
+        <div className='w-[100px] h-[100px] bg-blue-300 fixed left-[550px] top-[550px]'>  
+        </div>
+        <div className='w-[100px] h-[100px] bg-green-300'>  
+        </div>
+        <div className='w-[100px] h-[100px] absolute right-[20px] bottom-[20px] bg-pink-300'>  
+        </div>
+      </div>
+    </div>
   )
 }
 
---
+++ src/components/userData.jsx
@@ -0,0 +1,9 @@
+export default function UserData(props) {
+    return (
+        <div>
+            <img src="https://www.flaticon.com/free-icon/shopping-cart_3144456?term=cart&page=1&position=1&origin=search&related_id=3144456" alt="" />
+            <span>Cart (0)</span>
+            <p>Kamal</p>
+        </div>
+    )
+}
